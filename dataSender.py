b25f5fd0bf4302ba993b84be3048344d5cb50d0f
Ezra Sunshine <esunshin@bowdoin.edu>
2018-06-23 19:31:42 -0400

    Change to dummy url

--- dataSender.py
+++ dataSender.py
@@ -2,8 +2,8 @@ import requests as r
 
 
 def sendData(name, amount, note):
-    
+
     params = {"name": name, "amount": amount, "note": note}
-    resp = r.post('https://eatsmartcookie.com/resources/venmoPayment.php', data=params)
+    resp = r.post('https://yourSite.com/resources/acceptPayment.php', data=params)
     print(resp)
     print(resp.text[:300])
